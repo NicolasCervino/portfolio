d02cbdc3a8617d0ab6f4f80adef41fbaf0950d14
Nicolas Cervino <nicolascervino2@gmail.com>
2023-01-18 00:57:22 -0300

    se agrega el Navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Social from "./social/Social";
+
+function Navbar() {
+    const [nav, setNav] = useState<boolean>(false);
+
+    const handleClick = () => setNav(!nav);
+
+    return (
+        <div className="fixed w-full h-[80px] flex md:justify-center justify-end items-center px-4 bg-[#0a192f] text-gray-300">
+            {/* Menu */}
+            <ul className="hidden md:flex">
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Contact</li>
+            </ul>
+
+            {/* Hamburger */}
+            <div onClick={handleClick} className="md:hidden z-10 cursor-pointer">
+                {!nav ? <FaBars /> : <FaTimes />}
+            </div>
+
+            {/* Mobile menu */}
+            <ul
+                className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"}
+            >
+                <li className="py-6 text-4xl">Home</li>
+                <li className="py-6 text-4xl">About</li>
+                <li className="py-6 text-4xl">Skills</li>
+                <li className="py-6 text-4xl">Contact</li>
+            </ul>
+
+            {/* Social icons */}
+            <Social />
+        </div>
+    );
+}
+
+export default Navbar;
