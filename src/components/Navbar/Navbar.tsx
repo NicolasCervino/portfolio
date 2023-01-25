cc905b5b84c2a4ea515cd14f045b1f4c824484a5
Nicolas Cervino <nicolascervino2@gmail.com>
2023-01-25 18:20:15 -0300

    cambios menores

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,12 +8,13 @@ function Navbar() {
     const handleClick = () => setNav(!nav);
 
     return (
-        <div className="fixed w-full h-[80px] flex md:justify-center justify-end items-center px-4 bg-[#0a192f] text-gray-300">
+        <div className="fixed w-full h-[80px] flex md:justify-center justify-end items-center px-4">
             {/* Menu */}
             <ul className="hidden md:flex">
                 <li>Home</li>
                 <li>About</li>
                 <li>Skills</li>
+                <li>Work</li>
                 <li>Contact</li>
             </ul>
 
@@ -24,11 +25,16 @@ function Navbar() {
 
             {/* Mobile menu */}
             <ul
-                className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"}
+                className={
+                    !nav
+                        ? "hidden"
+                        : "absolute top-0 left-0 w-full h-screen bg-gradient-to-l from-[#34016b] to-[#00215a] flex flex-col justify-center items-center"
+                }
             >
                 <li className="py-6 text-4xl">Home</li>
                 <li className="py-6 text-4xl">About</li>
                 <li className="py-6 text-4xl">Skills</li>
+                <li className="py-6 text-4xl">Work</li>
                 <li className="py-6 text-4xl">Contact</li>
             </ul>
 
--- src/components/Navbar/social/SocialIcon.tsx
+++ src/components/Navbar/social/SocialIcon.tsx
@@ -1,12 +1,11 @@
 function SocialIcon({ name, icon, background, link }: { name: string; icon: any; background: string; link: string }) {
     return (
         <li className={`w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 ${background}`}>
-            <a className="flex justify-between items-center w-full text-gray-300" href={link} target="_blank" >
+            <a className="flex justify-between items-center w-full" href={link} target="_blank">
                 <>
                     {name} {icon}
                 </>
             </a>
-            
         </li>
     );
 }
