85606dea18f0c20716011b0f45d2c3098f785309
Nicolas Cervino <nicolascervino2@gmail.com>
2023-01-25 18:27:46 -0300

    se agrega Skills

---
+++ src/components/Skills.tsx
@@ -0,0 +1,58 @@
+import HTML from "../assets/html.png";
+import CSS from "../assets/css.png";
+import Javascript from "../assets/javascript.png";
+import React from "../assets/react.png";
+import Bootstrap from "../assets/bootstrap.png";
+import Tailwind from "../assets/tailwind.png";
+import Firebase from "../assets/firebase.png";
+import Git from "../assets/git.png";
+
+function Skills() {
+    return (
+        <div id="skills" className="w-full h-screen">
+            <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+                <div>
+                    <p className="text-4xl font-bold inline border-b-4 border-primary">Skills</p>
+                    <p className="py-4">// These are the technologies I've worked with</p>
+                </div>
+                {/* Technologies Imgs*/}
+                <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        <img className="w-20 mx-auto" src={HTML} alt="HTML icon"></img>
+                        <p className="my-4">HTML</p>
+                    </div>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        <img className="w-20 mx-auto" src={CSS} alt="CSS icon"></img>
+                        <p className="my-4">CSS</p>
+                    </div>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        <img className="w-20 mx-auto" src={Javascript} alt="JS icon"></img>
+                        <p className="my-4">Javascript</p>
+                    </div>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        <img className="w-20 mx-auto" src={React} alt="React icon"></img>
+                        <p className="my-4">React</p>
+                    </div>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        <img className="w-20 mx-auto" src={Bootstrap} alt="Bootstrap icon"></img>
+                        <p className="my-4">Bootstrap</p>
+                    </div>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        <img className="w-20 mx-auto" src={Tailwind} alt="Tailwind icon"></img>
+                        <p className="my-4">Tailwind</p>
+                    </div>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        <img className="w-20 mx-auto" src={Git} alt="Git icon"></img>
+                        <p className="my-4">Git</p>
+                    </div>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        <img className="w-20 mx-auto" src={Firebase} alt="Firebase icon"></img>
+                        <p className="my-4">Firebase</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Skills;
