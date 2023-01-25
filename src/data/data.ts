31d1016ad48b6bb3fe171b31f4dd5614c151f578
Nicolas Cervino <nicolascervino2@gmail.com>
2023-01-25 18:30:30 -0300

    se agregan imagenes y proyectos

---
+++ src/data/data.ts
@@ -0,0 +1,34 @@
+import Redragon from "../assets/proyects/redragon.jpg";
+import MoviesApp from "../assets/proyects/moviesApp.jpg";
+import MoviesAppReact from "../assets/proyects/moviesAppReact.jpg";
+import EgoLifting from "../assets/proyects/ego-lifting.jpg";
+
+export const data = [
+    {
+        id: 1,
+        name: "Redragon Clone",
+        image: Redragon,
+        links: {
+            github: "https://github.com/NicolasCervino/ProyectoFinalRedragon",
+            live: "https://nicolascervino.github.io/ProyectoFinalRedragon/",
+        },
+    },
+    {
+        id: 2,
+        name: "MoviesApp",
+        image: MoviesApp,
+        links: {
+            github: "https://github.com/NicolasCervino/ProyectoFinalMoviesApp",
+            live: "https://nicolascervino.github.io/ProyectoFinalMoviesApp/",
+        },
+    },
+    {
+        id: 3,
+        name: "MoviesApp React",
+        image: MoviesAppReact,
+        links: {
+            github: "https://github.com/NicolasCervino/movies-app-react",
+            live: "https://movies-app-react-beta.vercel.app/home",
+        },
+    },
+];
