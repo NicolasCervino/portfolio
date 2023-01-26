593064c2da5cbd58c62f32fadfceda701f0783d9
Nicolas Cervino <nicolascervino2@gmail.com>
2023-01-26 03:47:15 -0300

    se agrega los links a Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import Social from "./social/Social";
+import { Link } from "react-scroll";
 
 function Navbar() {
     const [nav, setNav] = useState<boolean>(false);
@@ -11,11 +12,31 @@ function Navbar() {
         <div className="fixed w-full h-[80px] flex md:justify-center justify-end items-center px-4">
             {/* Menu */}
             <ul className="hidden md:flex">
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Work</li>
-                <li>Contact</li>
+                <li>
+                    <Link to="home" smooth={true}>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to="about" smooth={true}>
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link to="skills" smooth={true}>
+                        Skills
+                    </Link>
+                </li>
+                <li>
+                    <Link to="work" smooth={true}>
+                        Work
+                    </Link>
+                </li>
+                <li>
+                    <Link to="contact" smooth={true}>
+                        Contact
+                    </Link>
+                </li>
             </ul>
 
             {/* Hamburger */}
@@ -31,11 +52,31 @@ function Navbar() {
                         : "absolute top-0 left-0 w-full h-screen bg-gradient-to-l from-[#34016b] to-[#00215a] flex flex-col justify-center items-center"
                 }
             >
-                <li className="py-6 text-4xl">Home</li>
-                <li className="py-6 text-4xl">About</li>
-                <li className="py-6 text-4xl">Skills</li>
-                <li className="py-6 text-4xl">Work</li>
-                <li className="py-6 text-4xl">Contact</li>
+                <li className="py-6 text-4xl">
+                    <Link to="home" smooth={true} onClick={handleClick}>
+                        Home
+                    </Link>
+                </li>
+                <li className="py-6 text-4xl">
+                    <Link to="about" smooth={true} onClick={handleClick}>
+                        About
+                    </Link>
+                </li>
+                <li className="py-6 text-4xl">
+                    <Link to="skills" smooth={true} onClick={handleClick}>
+                        Skills
+                    </Link>
+                </li>
+                <li className="py-6 text-4xl">
+                    <Link to="work" smooth={true} onClick={handleClick}>
+                        Work
+                    </Link>
+                </li>
+                <li className="py-6 text-4xl">
+                    <Link to="contact" smooth={true} onClick={handleClick}>
+                        Contact
+                    </Link>
+                </li>
             </ul>
 
             {/* Social icons */}
