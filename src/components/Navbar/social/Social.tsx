8cf9fe3ec6ad749d63b4b585c23841e250b47430
Nicolas Cervino <nicolascervino2@gmail.com>
2023-01-26 20:54:20 -0300

    descarga de cv funcionando

--- src/components/Navbar/social/Social.tsx
+++ src/components/Navbar/social/Social.tsx
@@ -2,6 +2,7 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import SocialIcon from "./SocialIcon";
+import Resume from "../../../assets/resume.pdf";
 
 function Social() {
     return (
@@ -12,25 +13,28 @@ function Social() {
                     icon={<FaLinkedin size={30} />}
                     background="bg-blue-600"
                     link="https://www.linkedin.com/in/nicolas-cervino/"
+                    download=""
                 />
                 <SocialIcon
                     name={"GitHub"}
                     icon={<FaGithub size={30} />}
                     background="bg-[#333333]"
                     link="https://github.com/NicolasCervino"
+                    download=""
                 />
                 <SocialIcon
                     name={"Email"}
                     icon={<HiOutlineMail size={30} />}
                     background="bg-[#6fc2b0]"
                     link="mailto:nicolascervino2@gmail.com"
+                    download=""
                 />
                 <SocialIcon
                     name={"Resume"}
                     icon={<BsFillPersonLinesFill size={30} />}
                     background="bg-[#565f69]"
-                    /* Link pendiente  */
-                    link="/" 
+                    link={Resume}
+                    download="Resume"
                 />
             </ul>
         </div>
--- src/components/Navbar/social/SocialIcon.tsx
+++ src/components/Navbar/social/SocialIcon.tsx
@@ -1,11 +1,31 @@
-function SocialIcon({ name, icon, background, link }: { name: string; icon: any; background: string; link: string }) {
+function SocialIcon({
+    name,
+    icon,
+    background,
+    link,
+    download,
+}: {
+    name: string;
+    icon: any;
+    background: string;
+    link: string;
+    download: string;
+}) {
     return (
         <li className={`w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 ${background}`}>
-            <a className="flex justify-between items-center w-full" href={link} target="_blank">
-                <>
-                    {name} {icon}
-                </>
-            </a>
+            {download ? (
+                <a className="flex justify-between items-center w-full" href={link} target="_blank" download={download}>
+                    <>
+                        {name} {icon}
+                    </>
+                </a>
+            ) : (
+                <a className="flex justify-between items-center w-full" href={link} target="_blank">
+                    <>
+                        {name} {icon}
+                    </>
+                </a>
+            )}
         </li>
     );
 }
