d187ae9455fc46f5ee55670aa81d6c418860df9c
Nicolas Cervino <nicolascervino2@gmail.com>
2023-01-26 17:43:28 -0300

    se agrega Element y Link de react scroll

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,28 +1,37 @@
 import { HiArrowNarrowRight } from "react-icons/hi";
+import { Element, Link } from "react-scroll";
 
 function Home() {
     return (
-        <div className="w-full h-screen">
-            {/* Container */}
-            <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-                <p className="text-primary">Hi, my name is</p>
-                <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6fc]">Nicolas Cervino</h1>
-                <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm a Front-End Developer</h2>
-                <p className="text-[#8892b0] py-4 max-w-[750px]">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dolor doloremque impedit voluptatum nesciunt a
-                    repellat praesentium provident numquam dicta quibusdam quo alias accusantium laboriosam distinctio natus, illum saepe
-                    adipisci.
-                </p>
-                <div>
-                    <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-primary hover:border-primary">
-                        View work
-                        <span className="group-hover:rotate-90 duration-300">
-                            <HiArrowNarrowRight className="ml-3" />
-                        </span>
-                    </button>
+        <Element name="home">
+            <div className="w-full h-screen">
+                {/* Container */}
+                <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+                    <p className="text-primary text-2xl">Hi, my name is</p>
+                    <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6fc]">Nicolas Cervino</h1>
+                    <h2 className="text-4xl sm:text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-teal-500 to-primary animate-text">
+                        I'm a Front-End Developer
+                    </h2>
+                    <p className="text-[#8892b0] py-4 max-w-[750px]">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dolor doloremque impedit voluptatum nesciunt a
+                        repellat praesentium provident numquam dicta quibusdam quo alias accusantium laboriosam distinctio natus, illum
+                        saepe adipisci.
+                    </p>
+                    <div>
+                        <Link
+                            to="work"
+                            smooth={true}
+                            className="w-fit cursor-pointer text-white group border-2 px-6 py-3 my-2 flex items-center transition-all duration-300 ease-out hover:bg-gradient-to-b from-teal-500  to-primary hover:border-primary"
+                        >
+                            View work
+                            <span className="ml-3 group-hover:rotate-90 duration-300 p-0">
+                                <HiArrowNarrowRight />
+                            </span>
+                        </Link>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Element>
     );
 }
 
