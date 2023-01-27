29e5e00306e04e0c30f109a07b71269dc5f3cddc
Nicolas Cervino <nicolascervino2@gmail.com>
2023-01-26 21:03:34 -0300

    se agrega Element de react-scroll

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,52 +6,55 @@ import Bootstrap from "../assets/bootstrap.png";
 import Tailwind from "../assets/tailwind.png";
 import Firebase from "../assets/firebase.png";
 import Git from "../assets/git.png";
+import { Element } from "react-scroll";
 
 function Skills() {
     return (
-        <div id="skills" className="w-full h-screen">
-            <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-                <div>
-                    <p className="text-4xl font-bold inline border-b-4 border-primary">Skills</p>
-                    <p className="py-4">// These are the technologies I've worked with</p>
-                </div>
-                {/* Technologies Imgs*/}
-                <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
-                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-                        <img className="w-20 mx-auto" src={HTML} alt="HTML icon"></img>
-                        <p className="my-4">HTML</p>
+        <Element name="skills">
+            <div className="w-full h-screen">
+                <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+                    <div>
+                        <p className="text-4xl font-bold inline border-b-4 border-primary">Skills</p>
+                        <p className="py-4">// These are the technologies I've worked with</p>
                     </div>
-                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-                        <img className="w-20 mx-auto" src={CSS} alt="CSS icon"></img>
-                        <p className="my-4">CSS</p>
-                    </div>
-                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-                        <img className="w-20 mx-auto" src={Javascript} alt="JS icon"></img>
-                        <p className="my-4">Javascript</p>
-                    </div>
-                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-                        <img className="w-20 mx-auto" src={React} alt="React icon"></img>
-                        <p className="my-4">React</p>
-                    </div>
-                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-                        <img className="w-20 mx-auto" src={Bootstrap} alt="Bootstrap icon"></img>
-                        <p className="my-4">Bootstrap</p>
-                    </div>
-                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-                        <img className="w-20 mx-auto" src={Tailwind} alt="Tailwind icon"></img>
-                        <p className="my-4">Tailwind</p>
-                    </div>
-                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-                        <img className="w-20 mx-auto" src={Git} alt="Git icon"></img>
-                        <p className="my-4">Git</p>
-                    </div>
-                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-                        <img className="w-20 mx-auto" src={Firebase} alt="Firebase icon"></img>
-                        <p className="my-4">Firebase</p>
+                    {/* Technologies Imgs*/}
+                    <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+                        <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                            <img className="w-20 mx-auto" src={HTML} alt="HTML icon"></img>
+                            <p className="my-4">HTML</p>
+                        </div>
+                        <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                            <img className="w-20 mx-auto" src={CSS} alt="CSS icon"></img>
+                            <p className="my-4">CSS</p>
+                        </div>
+                        <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                            <img className="w-20 mx-auto" src={Javascript} alt="JS icon"></img>
+                            <p className="my-4">Javascript</p>
+                        </div>
+                        <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                            <img className="w-20 mx-auto" src={React} alt="React icon"></img>
+                            <p className="my-4">React</p>
+                        </div>
+                        <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                            <img className="w-20 mx-auto" src={Bootstrap} alt="Bootstrap icon"></img>
+                            <p className="my-4">Bootstrap</p>
+                        </div>
+                        <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                            <img className="w-20 mx-auto" src={Tailwind} alt="Tailwind icon"></img>
+                            <p className="my-4">Tailwind</p>
+                        </div>
+                        <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                            <img className="w-20 mx-auto" src={Git} alt="Git icon"></img>
+                            <p className="my-4">Git</p>
+                        </div>
+                        <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                            <img className="w-20 mx-auto" src={Firebase} alt="Firebase icon"></img>
+                            <p className="my-4">Firebase</p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </Element>
     );
 }
 
--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,22 +1,25 @@
+import { Element } from "react-scroll";
 import { data } from "../data/data.js";
 import ProyectCard from "./ProyectCard";
 
 function Work() {
     return (
-        <div id="work" className="w-full md:h-screen">
-            <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-                <div className="pb-8">
-                    <p className="text-4xl font-bold inline border-b-4 border-primary">Work</p>
-                    <p className="py-4">// Check out some of my recent work</p>
-                </div>
-                {/* Proyects */}
-                <div className="grid md:grid-cols-2 gap-4">
-                    {data.map((p) => (
-                        <ProyectCard key={p.id} name={p.name} background={p.image} links={p.links} />
-                    ))}
+        <Element name="work">
+            <div className="w-full md:h-screen">
+                <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+                    <div className="pb-8">
+                        <p className="text-4xl font-bold inline border-b-4 border-primary">Work</p>
+                        <p className="py-4">// Check out some of my recent work</p>
+                    </div>
+                    {/* Proyects */}
+                    <div className="grid md:grid-cols-2 gap-4">
+                        {data.map((p) => (
+                            <ProyectCard key={p.id} name={p.name} background={p.image} links={p.links} />
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
+        </Element>
     );
 }
 
