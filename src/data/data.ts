2ec3736bd2e8ab3b67dbe453230005ea4957f62c
Nicolas Cervino <99894603+NicolasCervino@users.noreply.github.com>
2023-03-28 22:49:54 -0300

    added new proyect

--- src/data/data.ts
+++ src/data/data.ts
@@ -4,31 +4,40 @@ import MoviesAppReact from "../assets/proyects/moviesAppReact.jpg";
 import EgoLifting from "../assets/proyects/ego-lifting.jpg";
 
 export const data = [
-    {
-        id: 1,
-        name: "Redragon Clone",
-        image: Redragon,
-        links: {
-            github: "https://github.com/NicolasCervino/ProyectoFinalRedragon",
-            live: "https://nicolascervino.github.io/ProyectoFinalRedragon/",
-        },
+  {
+    id: 1,
+    name: "Redragon Clone",
+    image: Redragon,
+    links: {
+      github: "https://github.com/NicolasCervino/ProyectoFinalRedragon",
+      live: "https://nicolascervino.github.io/ProyectoFinalRedragon/",
     },
-    {
-        id: 2,
-        name: "MoviesApp",
-        image: MoviesApp,
-        links: {
-            github: "https://github.com/NicolasCervino/ProyectoFinalMoviesApp",
-            live: "https://nicolascervino.github.io/ProyectoFinalMoviesApp/",
-        },
+  },
+  {
+    id: 2,
+    name: "MoviesApp",
+    image: MoviesApp,
+    links: {
+      github: "https://github.com/NicolasCervino/ProyectoFinalMoviesApp",
+      live: "https://nicolascervino.github.io/ProyectoFinalMoviesApp/",
     },
-    {
-        id: 3,
-        name: "MoviesApp React",
-        image: MoviesAppReact,
-        links: {
-            github: "https://github.com/NicolasCervino/movies-app-react",
-            live: "https://movies-app-react-beta.vercel.app/home",
-        },
+  },
+  {
+    id: 3,
+    name: "MoviesApp React",
+    image: MoviesAppReact,
+    links: {
+      github: "https://github.com/NicolasCervino/movies-app-react",
+      live: "https://movies-app-react-beta.vercel.app/home",
     },
+  },
+  {
+    id: 4,
+    name: "Ego-Lifting",
+    image: EgoLifting,
+    links: {
+      github: "https://github.com/NicolasCervino/GymApp",
+      live: "https://ego-lifting.vercel.app/",
+    },
+  },
 ];
